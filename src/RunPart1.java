38fbfcf14c74c619c4688f271a74d5c7d32b707a
ramcode <ramesh.thetakali@gmail.com>
2016-10-13 19:14:54 -0400

    added main classes

---
+++ src/RunPart1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by VenkataRamesh on 10/13/2016.
+ */
+public class RunPart1 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/RunPart2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by VenkataRamesh on 10/13/2016.
+ */
+public class RunPart2 {
+
+    public static void main(String[] args) {
+
+    }
+}
